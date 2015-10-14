1950191d1b0b225fcfc980f97b90a5fe194938de
tschaef <tschaef1@gwdg.de>
2015-10-14 13:44:39 +0200

    Display variants and all metadata (WIP)
    - Change Solr query to include variants
    - Parse letter fulltext and prefix classes
    - Make dynamic internal links functional
    
    Styles:
    - Add SCSS mixins for buttons and links
    - Change color of loading spinner
    - Make header smaller
    - Add Sass-generated SVG button
    - Update icon mixin for positioning on the right
    - Change mixin text-color to a function
    - Move shade and tint functions to correct file
    - Add function to convert short-hex color to long format for SVG rendering
    
    Templates:
    - Add variants template
    - Add helper to allow comparisons in hbs if
    - Split up letter template
    - Update translation
    - Add toggleable metadata box
    
    Related to: ADWD-1823, ADWD-1892, ADWD-1789

--- app/routes/letter.js
+++ app/routes/letter.js
@@ -7,14 +7,19 @@ export default Ember.Route.extend({
       url: config.solrURL,
       data: {
         wt: 'json',
-        q: 'id:' + params.letter_id
+        q: `id:${params.letter_id} or (doc_id:${params.letter_id} and type:variante)`
       },
       dataType: 'jsonp',
       jsonp: 'json.wrf'
     }).then(function(json) {
       var letter = {};
       if ( json.response.docs.length > 0 ) {
+        // TODO: Is letter always the first doc in response?
         letter = json.response.docs[0];
+        letter.variants = json.response.docs.slice(1);
+        // Add class prefix to all elements
+        // NOTE: This only works for single class names
+        letter.volltext = letter.volltext.replace(/class="([a-z0-9_-]+)"/g, 'class="letter_$1"');
       } else {
         letter.id = params.letter_id;
       }
@@ -40,6 +45,16 @@ export default Ember.Route.extend({
   },
   afterModel: function() {
     Ember.run.next( () => {
+      // Turn static links into ember transition links
+      var route = this;
+      // TODO: Get a elements directly
+      Ember.$('.metadata').find('a').click( function(e) {
+        e.preventDefault();
+        var letterID = Ember.$(this).attr('href');
+        route.transitionTo('letter', letterID);
+      });
+
+      // Render letter with MathJax
       if ( this.context.volltext ) {
         MathJax.Hub.Queue(['Typeset', MathJax.Hub], () => { // jshint ignore:line
           this.controller.set('rendered', true);
