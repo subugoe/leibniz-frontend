50be163d3d83b7cc258e03fc7c3409075c3d7f3b
tschaef <tschaef1@gwdg.de>
2015-10-15 15:19:14 +0200

    Change variant display, harmonize DOM of lanes
    - Display only variant text and textual witness
    - Update translation
    - Add lane type to class
    - Move letter rendering into own functions

--- app/components/pop-up.js
+++ app/components/pop-up.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: 'popup',
   tagName: 'span',
-
   didInsertElement: function() {
     var $this = this.$()
     if ( this.get('wide') ) {
