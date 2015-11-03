2111dd938ee5d76c58c8375e38d99b937f77a1d4
tschaef <tschaef1@gwdg.de>
2015-11-03 14:22:50 +0100

    Improve performance when resizing content
    Redraw variants only after transitions have completed and make sure
    that click triggers are only bound once.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,15 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  showNav: false,
   actions: {
     toggleNav: function() {
       this.set('showNav', ! this.get('showNav'));
-      // Remove and redraw variant connectors
-      this.send('beforeResize');
-      Ember.$('.main').on('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', () => {
-        this.send('afterResize');
-      });
     }
   },
+  showNav: false
 });
