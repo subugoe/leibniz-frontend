017a8d3bf9c8747ae38aa1fc92c689c70cd701a9
tschaef <tschaef1@gwdg.de>
2015-11-04 14:57:00 +0100

    Display correspondents string in header
    - Move sender and recipient info to popup
    - Remove `wide` option from popups, use nowrap and manual breaks instead
    - Use <time> for dates
    - Clean up header styles

--- app/components/pop-up.js
+++ app/components/pop-up.js
@@ -5,9 +5,6 @@ export default Ember.Component.extend({
   tagName: 'span',
   didInsertElement: function() {
     var $this = this.$();
-    if ( this.get('wide') ) {
-      $this.addClass('-wide');
-    }
     var $parent = $this.parent();
     $parent.css('position', 'relative');
     if ( $parent.offset().left > 0.75 * Ember.$(window).width() ) {
