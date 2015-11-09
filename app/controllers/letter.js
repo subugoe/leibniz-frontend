05f9bff09e8efe911d5055de72ee307df73833f9
tschaef <tschaef1@gwdg.de>
2015-11-09 19:44:31 +0100

    Add lane headers
    Dynamically set header content depending on lane type. Make sure translation
    is up to date when adding more types.
    
    Add a general lane template and load the actual display component from inside this
    template instead of letting the parent controller handling templates.
    
    Make popup styles more reusable by adding an extend.
    
    Includes prelimiary actions for lane type switching which will be implemented
    at a later stage.
    
    Related to: ADWD-1967

--- app/controllers/letter.js
+++ app/controllers/letter.js
@@ -2,5 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   queryParams: ['view'],
+  laneTypes: [
+    'scans',
+    'transcript',
+    'variants'
+  ],
   view: '{"lanes":[{"type":"transcript","width":50},{"type":"variants"}]}'
 });
