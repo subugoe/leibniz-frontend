b1ead8980ae59555d2c81525acaed10d9073b6c1
tschaef <tschaef1@gwdg.de>
2015-12-07 16:11:59 +0100

    Fix highlighting of references when toggling variants
    Remove highlight from associated reference when variant is hidden.
    
    Update loader.js to get rid of deprecation notice, fix test file name.
    
    Resolves: ADWD-2003

--- tests/acceptance/toggle-variants-test.js
+++ tests/acceptance/toggle-variants-test.js
@@ -5,17 +5,26 @@ moduleForAcceptance('Acceptance | toggle variants test');
 
 // TODO: Make this test more elaborate
 test('toggle variants', function(assert) {
+  assert.expect(2);
+
   // Make sure this letter actually has variants
   visit('/letter/l3717');
 
   var variantsCount = 0
+  var highlightsCount = 0
+
+  // Highlight all variants
+  click('.variants .variant');
   andThen(function() {
     variantsCount = find('.variants .variant:visible').length;
+    highlightsCount = find('.transcript .reference.-highlight').length;
   })
 
   click('.variants .variants_button:first');
   andThen(function() {
     var visibleVariantsCount = find('.variants .variant:visible').length;
-    assert.ok(visibleVariantsCount < variantsCount, 'some variants should be hidden');
+    var lessHighlightsCount = find('.transcript .reference.-highlight').length;
+    assert.ok(visibleVariantsCount < variantsCount, 'less variants should be visible');
+    assert.ok(lessHighlightsCount < highlightsCount, 'less references should be highlighted')
   });
 });
