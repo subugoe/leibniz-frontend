fbfe8dc0a4899b622d16e69f427e554ba636e16e
tschaef <tschaef1@gwdg.de>
2016-04-21 18:42:33 +0200

    WIP: Add bibliographic data, display child variants
    This is a work in progress adding two major features:
    
    1. Loading and displaying of bibliographic data
    
       This should be complete, but is untested.
    
    2. Displaying child variants (aka footnotes in footnotes)
    
       Child variants are displayed at the correct position, but
       neither is the reference in the parent highlighted, nor
       is a connector line drawn.
    
    Your mission, should you choose to accept it, is to firstly
    rebase this branch onto develop after all open pull requests
    have been merged (there shouldn't be any conflicts).
    
    Then add the missing features along with E2E tests, create new
    branches and split the code into as many commits as reasonable.
    When done, delete this branch - do not merge directly.
    
    As always, should you or any of your team be caught or killed,
    the Secretary will disavow any knowledge of your actions.
    This message will self-destruct in five seconds. Good luck.

--- app/routes/letter.js
+++ app/routes/letter.js
@@ -57,14 +57,29 @@ export default Ember.Route.extend(Solr, {
     controller.set('rendered', true);
   },
   afterModel() {
+    var $ = Ember.$;
+
     Ember.run.next( () => {
       this.activateLinks();
 
+      // Add bibliographic info popups
+      $('.metadata').find('.reference.-biblio').each( (index, ref) => {
+        var id = $(ref).data('href');
+        this.loadBiblio(id).then( function(html) {
+          // TODO: This is not an instance of "pop-up" component, but merely
+          // a dumb DOM node with he same class name. Therefore, position and
+          // size are not adjusted automatically.
+          var $popup = $('<span class="pop-up -wide -right"/>');
+          $popup.html(html);
+          $(ref).css('position', 'relative').append($popup);
+        });
+      });
+
       // Convert image references to SVG images
-      Ember.$('.transcript, .variants').find('.reference.-image').each( (index, ref) => {
-        var imageID = Ember.$(ref).data('id');
-        this.loadSVG(imageID).then( function(svg) {
-          Ember.$(ref).html(svg);
+      $('.transcript, .variants').find('.reference.-image').each( (index, ref) => {
+        var id = $(ref).data('id');
+        this.loadSVG(id).then( function(svg) {
+          $(ref).html(svg);
         });
       });
 
@@ -72,7 +87,7 @@ export default Ember.Route.extend(Solr, {
       this.renderMathJax().then( () => {
         // TODO: For some reason, resize is always triggered once
         // this.positionVariants();
-        Ember.$('.lane').resize( () => {
+        $('.lane').resize( () => {
           Ember.run.debounce(this, this.clearVariantConnectors, 333, true);
           Ember.run.debounce(this, this.positionVariants, 333);
         });
@@ -90,10 +105,19 @@ export default Ember.Route.extend(Solr, {
       route.transitionTo('letter', letterID);
     });
   },
+  loadBiblio(id) {
+    return new Ember.RSVP.Promise( (resolve) => {
+      this.query(`id:${id}`).then( function(json) {
+        if ( json.response.docs.length > 0 && json.response.docs[0].hasOwnProperty('bibliographische_angabe') ) {
+          resolve(json.response.docs[0].bibliographische_angabe);
+        }
+      });
+    });
+  },
   loadSVG(id) {
     return new Ember.RSVP.Promise( (resolve) => {
       this.query(`id:${id}`).then( function(json) {
-        if ( json.response.docs.length > 0 && json.response.docs[0].hasOwnProperty('svg_code')) {
+        if ( json.response.docs.length > 0 && json.response.docs[0].hasOwnProperty('svg_code') ) {
           resolve(json.response.docs[0].svg_code);
         }
       });
@@ -116,18 +140,18 @@ export default Ember.Route.extend(Solr, {
   },
   positionVariants() {
     var $ = Ember.$;
-    var $laneTranscript = $('.transcript');
-    var $references = $laneTranscript.find('.reference');
+    var $transcriptLane = $('.transcript');
+    var $references = $transcriptLane.find('.reference.-afootnote, .reference.-cfootnote');
 
     this.clearVariantConnectors();
     if ( $references.length === 0 ) {
       return;
     }
 
-    var $laneVariants = $('.variants');
-    var $laneHeader = $laneVariants.find('.lane_header');
-    var $variants = $laneVariants.find('.variant');
-    var marginBetweenVariants = parseInt( $laneVariants.css('line-height') ) / 2;
+    var $variantsLane = $('.variants');
+    var $laneHeader = $variantsLane.find('.lane_header');
+    var $variants = $variantsLane.find('.variant');
+    var marginBetweenVariants = parseInt( $variantsLane.css('line-height') ) / 2;
     var prevVariantBottom = $laneHeader.position().top + $laneHeader.outerHeight();
     // NOTE: Shorthand css properties like `padding` are not supported in Firefox
 
@@ -144,7 +168,7 @@ export default Ember.Route.extend(Solr, {
       var variantID = $this.data('id');
       var $variant = $variants.filter('#' + variantID);
       if ( $variant.length === 0 ) {
-        return; // variant not available
+        return; // no variant with this ID
       }
       var left = $this.position().left;
       var top = $this.position().top;
@@ -162,9 +186,9 @@ export default Ember.Route.extend(Solr, {
       path.setAttribute('style', 'stroke-width: 1px');
       // 14: padding right of .lane_content
       var pathD = `M ${left + 3}, ${bottom - .5}
-                   L ${$laneTranscript.width() - 14}, ${bottom - .5}
-                   C ${$laneTranscript.width()}, ${bottom - .5},
-                     ${$laneTranscript.width()}, ${variantTop + $variant.outerHeight() / 2 - .5},
+                   L ${$transcriptLane.width() - 14}, ${bottom - .5}
+                   C ${$transcriptLane.width()}, ${bottom - .5},
+                     ${$transcriptLane.width()}, ${variantTop + $variant.outerHeight() / 2 - .5},
                      ${$variant.offset().left}, ${variantTop + $variant.outerHeight() / 2 - .5}`;
       path.setAttribute('d', pathD);
       svg.appendChild(path);
@@ -185,9 +209,18 @@ export default Ember.Route.extend(Solr, {
       });
 
       prevVariantBottom = variantTop + $variant.outerHeight() + marginBetweenVariants;
+
+      // TODO: Child variants / variants in variants
+      var $childReferences = $variant.find('.reference.-afootnote, .reference.-cfootnote');
+      $childReferences.each( function() {
+        var childVariantID = $(this).data('id');
+        var $childVariant = $variants.filter('#' + childVariantID);
+        $childVariant.css( {top: prevVariantBottom, marginLeft: '30px' /* TODO: use class instead */} ).addClass('-visible');
+        prevVariantBottom += $childVariant.outerHeight() + marginBetweenVariants;
+      });
     });
 
-    $laneVariants.height(prevVariantBottom);
+    $variantsLane.height(prevVariantBottom);
   }
 });
 
