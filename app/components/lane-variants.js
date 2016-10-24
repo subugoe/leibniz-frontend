e6e030dd6760a8e97631ee7f61d83bcc9ff59a67
snaegle <naegle@sub.uni-goettingen.de>
2016-10-24 23:05:43 +0200

    Fix other-button hides all variants error Resolves: ADWD-2386
    De-highlight references if variants are toggled
    Related to: ADWD.2386

--- app/components/lane-variants.js
+++ app/components/lane-variants.js
@@ -4,7 +4,12 @@ import Lane from '../mixins/lane';
 export default Ember.Component.extend(Lane, {
   otherVariantsVisible: true,
   actions: {
-    toggleVariants(witnessId) {
+    toggleVariants() {
+      this.toggleVariant();
+    },
+    toggleWitness(witnessId) {
+      // determine whether or not the button for the witness is shown
+
       if ( witnessId === 'other' ) {
         this.set('otherVariantsVisible', ! this.get('otherVariantsVisible'));
       } else {
@@ -15,40 +20,52 @@ export default Ember.Component.extend(Lane, {
           }
         });
       }
-      this.get('content.variants').forEach( (item, index, variants) => {
-        var variant = variants.objectAt(index);
-        if ( variant.textzeuge && variant.textzeuge.length > 1) {
-          // if variants have more than one witness
-          // they are shown when the current witness is not active (it will be after this function) or at least one other is active
-          // they are not shown when the current witness is active and no other is active
-          let showVariant = false;
-          variant.textzeuge.forEach( function(elem) {
-            if ( elem === witnessId ) {
-              if ( ! Ember.$('.variants_button:contains("'+elem+'")').hasClass('-active') ) {
-                showVariant = true;
-              }
-            } else {
-              if ( Ember.$('.variants_button:contains("'+elem+'")').hasClass('-active') ) {
-                showVariant = true;
-              }
-            }
-          });
-          return Ember.set(variant, 'visible', showVariant);
-        } else {
-          var toggleIndexedAndHasIndexedWitness = ( variant.textzeuge && variant.textzeuge[0] === witnessId );
-          var toggleOtherAndHasOtherWitness = ( witnessId === 'other' && ! variant.witnessIndex );
-          if ( toggleIndexedAndHasIndexedWitness || toggleOtherAndHasOtherWitness ) {
-            if ( variant.visible ) {
-              var $references = Ember.$('.lane.transcript').find(`[data-id=${variant.id}], [data-ref-id=${variant.id}]`);
-              $references.removeClass('-highlight');
-            }
-            return Ember.set(variant, 'visible', ! variant.visible);
-          }
-        }
-      });
+
       Ember.run.scheduleOnce('afterRender', () => {
-        this.sendAction('positionVariants');
+        this.toggleVariants();
       });
     }
+  },
+  toggleVariants() {
+    // variants are shown when:
+    // one of their witnesses is active, or
+    // it has others or no witness and the "others"-button is active
+
+    this.get('content.variants').forEach((item, index, variants) => {
+      var showVariant = false;
+      var variant = variants.objectAt(index);
+      if (variant.textzeuge) {
+        variant.textzeuge.forEach((elem) => {
+          let hasWitnessButtons = Ember.$('.variants .variants_button:contains("'+elem+'")');
+          if (hasWitnessButtons.length > 0) {
+            // witness has a button
+            hasWitnessButtons.each((index,elem) => {
+              if(Ember.$(elem).hasClass('-active')) {
+                showVariant = true;
+              }
+            });
+          } else {
+            // witness has no button
+            if (Ember.$('.variants .variants_button:contains("Sonstige")').hasClass('-active')) {
+              showVariant = true;
+            }
+          }
+        });
+      } else {
+        // variant doesn't have a witness and is only shown when "others" are active
+        if (Ember.$('.variants .variants_button:contains("Sonstige")').hasClass('-active')) {
+          showVariant = true;
+        }
+      }
+      if (showVariant === false) {
+        // respecting reference has to be de-highlighted
+        var $references = Ember.$('.lane.transcript').find(`[data-id=${variant.id}], [data-ref-id=${variant.id}]`);
+        $references.removeClass('-highlight');
+      }
+      return Ember.set(variant, 'visible', showVariant);
+    });
+    Ember.run.scheduleOnce('afterRender', () => {
+      this.sendAction('positionVariants');
+    });
   }
 });
